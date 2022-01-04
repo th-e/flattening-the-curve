1d51fc0ee85ed036870321e0d5d5cc3cf698b08b
Thomas Effing <thomas.effing@sbb.ch>
2022-01-04 16:25:16 +0100

    password from CLI

--- backend/src/main/scala/webtuples/Config.scala
+++ backend/src/main/scala/webtuples/Config.scala
@@ -4,17 +4,36 @@ import zio._
 import zio.config._
 import zio.config.magnolia._
 import zio.config.typesafe._
+import zio.config.magnolia.DeriveConfigDescriptor.descriptor
+import com.typesafe.config.ConfigFactory
 
 import java.io.File
 
-case class Config(version: String)
+case class Config(version: String, adminPassword: String)
 
 object Config {
   val descriptor: ConfigDescriptor[Config] =
     DeriveConfigDescriptor.descriptor[Config]
 
-  val live: ZLayer[system.System, Nothing, Has[Config]] =
-    TypesafeConfig.fromHoconFile(new File("application.conf"), descriptor).orDie
+  val loadConfigDescriptor: ZIO[ZEnv, ReadError[String], ConfigDescriptor[Config]] = {
+
+    val configDescriptor: ConfigDescriptor[Config] =
+      DeriveConfigDescriptor.descriptor[Config]
+
+    for {
+      envConfigSource <- ConfigSource.fromSystemEnv(
+        keyDelimiter = Some('_'),
+        valueDelimiter = Some(',')
+      )
+      typesafeConfigSource <- IO.fromEither(TypesafeConfigSource.fromHoconFile(new File("application.conf")))
+      sources: ConfigSource = envConfigSource <> typesafeConfigSource
+    } yield {
+      configDescriptor.updateSource(_ => sources)
+    }
+  }
+
+  val live: ZLayer[ZEnv, ReadError[String], Has[Config]] =
+    loadConfigDescriptor.map(read[Config](_)).absolve.toLayer
 
   val service: URIO[Has[Config], Config] = ZIO.service[Config]
 }
